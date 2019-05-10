ee364f7288b50d8b9059b98eb237f838a4b67986
kedar8 <kedar.kokil@metamagic.in>
2019-05-10 19:48:08 +0530

    menu: creative added.

---
+++ src/app/home/creative-menu1/menu.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { AmexioGridLayoutService, GridConfig, GridConstants } from 'amexio-ng-extensions';
+
+@Component({
+  selector: 'menu-one',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuOneComponent implements OnInit {
+
+  amexiotechmenus: any;
+
+  navmenus: any;
+
+  gridDesktop: GridConfig;
+  tempData: any;
+  facebookLoginData: any;
+  constructor(private _gridlayoutService: AmexioGridLayoutService) {
+    this.createLayouts();
+
+    // Create the Layouts
+    this._gridlayoutService.createLayout(this.gridDesktop);
+  }
+
+  createLayouts() {
+    this.gridDesktop = new GridConfig('LayoutSample1', GridConstants.Desktop)
+      .addlayout(["gridmenu1", "gridmenu2", "gridmenu3", "gridlogin"])
+      .addlayout(["gridmenu4", "gridmenu5", "gridmenu6", "gridlogin1"])
+
+    this.tempData = [
+      {
+        label: "About"
+
+      },
+      {
+        label: "Illustration"
+
+      },
+      {
+        label: "Photography"
+
+      },
+      {
+        label: "Web Design"
+
+      },
+      {
+        label: "Personal Project"
+
+      },
+      {
+        label: "Contact"
+
+      }
+
+    ]
+
+  }
+  onLoginClick(data: any) {
+    this.facebookLoginData = data;
+  }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,32 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {AmexioWidgetModule} from 'amexio-ng-extensions';
+import { AmexioWidgetModule } from 'amexio-ng-extensions';
 import { ViewportComponent } from './viewport/viewport.component';
 import { ViewportComponent2 } from './viewport2/viewport2.component';
-import {ViewportComponent3} from './viewport3/viewport3.component';
-import {RouterModule} from '@angular/router';
+import { ViewportComponent3 } from './viewport3/viewport3.component';
+import { RouterModule } from '@angular/router';
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { CommonModule } from '@angular/common';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { MenuOneComponent } from './creative-menu1/menu.component';
+import { MenuTwoComponent } from './creative-menu2/menu.component';
+import { MenuThreeComponent } from './creative-menu3/menu.component';
 const ROUTE: Routes = [
 
-    {
-        path: '', component: HomeComponent,
-        children: [
-          { path: '', redirectTo: 'scrollable-viewport', pathMatch: 'full' },
-          { path: 'scrollable-viewport', component: ViewportComponent },
-          { path: 'viewport', component: ViewportComponent2 },
-          { path: 'video-viewport', component: ViewportComponent3 }
-        ]
-        }
+  {
+    path: '', component: HomeComponent,
+    children: [
+      { path: '', redirectTo: 'scrollable-viewport', pathMatch: 'full' },
+      { path: 'scrollable-viewport', component: ViewportComponent },
+      { path: 'viewport', component: ViewportComponent2 },
+      { path: 'video-viewport', component: ViewportComponent3 },
+      { path: 'menu-one', component: MenuOneComponent },
+      { path: 'menu-two', component: MenuTwoComponent },
+      { path: 'menu-three', component: MenuThreeComponent },
+
+    ]
+  }
 ];
 @NgModule({
   declarations: [
     HomeComponent,
     ViewportComponent,
     ViewportComponent3,
-    ViewportComponent2
+    ViewportComponent2,
+    MenuOneComponent,
+    MenuTwoComponent,
+    MenuThreeComponent
   ],
   imports: [
     CommonModule, AmexioWidgetModule, RouterModule.forChild(ROUTE), FormsModule
